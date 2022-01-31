1c178c85d95e8d2d42f124d3249260e654d9ebeb
Seif Ibrahim <73166815+Dreamsan@users.noreply.github.com>
2022-01-31 22:20:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+void horizontal(int i){
+    for (int x=0;x<i;x++){
+        printf(" *");
+    }
+}
+void Vertical(int i){
+
+    for (int x=0;x<i;x++){
+        printf("*\n");
+    }
+
+}
+void Triangle(int r){
+   int i, o;
+   for (i = 1; i <= r; ++i) {
+      for (o= 1;o<= i; ++o) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+}
+
+void Rectangle(int r){
+
+
+int i, o;
+
+    for(i = 0; i < r; i++)
+    {
+        for(o = 0; o < r; o++)
+	{
+           printf("* ");
+        }
+        printf("\n");
+    }
+
+}
+
 
 int main()
 {
-    int a,s;
-    printf("Please enter your destination\n");
-    printf("1-First Floor\n");
-     printf("2-Second Floor\n");
-       printf("3-Third Floor\n");
-    scanf("%d",&a);
-    switch(a){
-    case 1: printf("you're heading towards the First floor");break;
-    case 2:if(a=2){
-        printf("Which section would you like to choose?\n");
-        printf("1-Section One\n");
-        printf("2-Section Two\n");
-        scanf("%d",&s);
-        switch(s){
-        case 1:printf("you're heading towards the Second floor Section 1");break;
-        case 2:printf("you're heading towards the Second floor Section 2");break;
-        default: printf("Wrong selection");break;
-        }
-
-    }
-
-    break;
-    case 3: printf("you're heading towards the Third floor");break;
-    default: printf("Wrong section");break;
-
-    }
-
+    printf("Please Enter the Number of the stars you want to make shapes with:\n");
+    int x;
+    scanf("%x",&x);
+    printf("The horizontal Line :");
+    horizontal(x);
+    printf("\n----------------------------------------------------------------------------------------------------------------------\n");
+    printf("\n The Vertical Line :\n");
+    Vertical(x);
+    printf("\n----------------------------------------------------------------------------------------------------------------------\n");
+    printf("\n The Triangle :\n");;
+    Triangle(x);
+    printf("\n----------------------------------------------------------------------------------------------------------------------\n");
+    printf("The Rectangle :\n");
+    Rectangle(x);
+    printf("\n----------------------------------------------------------------------------------------------------------------------\n");
+    printf("\tThanks for using my program and Have a Good day!");
+    printf("\n\t\tCredits: Seif Ibrahim");
+    printf("\n\t\tProf: Jalal Kiswani and Eng: Hana Alrasheed\n\n\n ");
 
     return 0;
 }
